a0e7ab81baee67e37c5d0e70e5409b5580f3ccb0
stap780 <panaet80@mail.ru>
2019-04-01 10:21:01 +0300

    change pict

--- app/models/itemimage.rb
+++ app/models/itemimage.rb
@@ -4,8 +4,8 @@ class Itemimage < ActiveRecord::Base
 	belongs_to :item
 
 	has_attached_file :image, styles: { medium: "600x600>", thumb: "100x100>" },
-    :path => ":rails_root/public/images/itemimages/:id/:style/:basename.:extension",
-    :url  => "/images/itemimages/:id/:style/:basename.:extension"
+    :path => ":rails_root/public/system/images/itemimages/:id/:style/:basename.:extension",
+    :url  => "/system/images/itemimages/:id/:style/:basename.:extension"
 
 	validates_attachment :image,
                      content_type: { content_type: /\Aimage\/.*\z/ },
